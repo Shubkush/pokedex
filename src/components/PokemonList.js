ca969ece2b521d8ba58082f5904d9dfa3e91f65c
shubhendu <shubhendu.kushwah@gmail.com>
2021-10-04 14:55:42 +0530

    Complete pagination

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,29 +1,74 @@
 import React, { useState, useEffect } from "react";
 import Grid from '@mui/material/Grid';
+import Pagination from '@mui/material/Pagination';
 import ImgMediaCard from '../materalui_components/card.js'
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+import PositionedMenu from '../materalui_components/positioned_menu';
 
 const PokemonList = () => {
-    const [pokemons, set_pokemon] = useState([]);
+    const [pokemons, setPokemon] = useState([]);
+    const [offset, setOffset] = useState(0);
+    const [limit, setLimit] = useState(20);
+    const [is_loading, setIsLoading] = useState(false);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
+        setIsLoading(true)
         const axios = require("axios");
+        let url = `/pokemon/?offset=${offset}&limit=${limit}`
+        console.log(offset, limit)
         axios
-            .get("/pokemon/").then(response => {
-                set_pokemon(response.data.results)
+            .get(url).then(response => {
+                setPokemon(response.data.results)
+                setIsLoading(false)
             })
-    }, []);
+    }, [offset, limit]);
 
+    const handlePangeChange = (event, value) => {
+        setOffset((value - 1) * 20)
+        setPage(value)
+    };
 
     return (
         <div>
-            <Grid container spacing={2}>
 
-                {pokemons.map((pokemon, idx) => (
-                    <Grid item xs={3}>
-                        <ImgMediaCard key={idx} data={{ name: pokemon.name, url: pokemon.url }} />
-                    </Grid>
-                ))}
-            </Grid>
+            {
+                is_loading ? (
+                    <LinearProgress />
+                )
+                    : (
+                        <div>
+
+                            <Box
+                                display="flex"
+                                justifyContent="right"
+                                alignItems="right"
+                                minHeight="10vh"
+                            >
+                                <Pagination page={page} count={10} color="primary" onChange={handlePangeChange} />
+                                <PositionedMenu onChange={(value) => setLimit(value)}
+                                    data={{ title: "Page Size", items: [10, 20, 50] }} />
+                                <p>{limit}</p>
+                            </Box>
+                            <Grid container spacing={5}>
+
+                                {pokemons.map((pokemon, idx) => (
+                                    <Grid item xs={4} key={idx + pokemon.name}>
+                                        <ImgMediaCard data={{ name: pokemon.name, url: pokemon.url }} />
+                                    </Grid>
+                                ))}
+                            </Grid>
+                            <Box
+                                display="flex"
+                                justifyContent="center"
+                                alignItems="center"
+                                minHeight="10vh"
+                            >
+                                <Pagination count={10} color="primary" onChange={handlePangeChange} />
+                            </Box>
+                        </div >
+                    )}
         </div >)
 }
 
--- src/materalui_components/card.js
+++ src/materalui_components/card.js
@@ -7,36 +7,40 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
+
 export default function ImgMediaCard(props) {
     const [image, set_image] = useState();
-
+    const [is_loading, set_isloading] = useState(false)
     useEffect(() => {
+        set_isloading(true)
         const axios = require("axios");
         axios
             .get(props.data.url).then(response => {
                 set_image(response.data.sprites.other["official-artwork"].front_default)
+                set_isloading(false)
             })
     }, []);
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                component="img"
-                alt="green iguana"
-                height="280"
-                image={image}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {props.data.name.charAt(0).toUpperCase() + props.data.name.slice(1)}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {/* {props.decs} */}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>
-    );
+        is_loading ? (<img src={require("../assets/loading.gif")} />) : (
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    component="img"
+                    alt={require("../assets/loading.gif")}
+                    height="330"
+                    image={image}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {props.data.name.charAt(0).toUpperCase() + props.data.name.slice(1)}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {/* {props.decs} */}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card >
+        ));
 }
